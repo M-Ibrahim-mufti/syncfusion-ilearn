2f2149fd2b6c0b61d700a21f97aa19b39213b0f6
Muhammad Awais <otechdev02@gmail.com>
2024-07-03 14:52:55 +0500

    add class metaData Table

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCeUx1RHxbf1x0ZFRGal5TTnVaUiweQnxTdEFjXn9ccHFQTmFdU0xwWQ==');
 
 if (environment.production) {
   enableProdMode();
